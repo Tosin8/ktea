15a971a81e052ed9d3496fe8b0766ed90d24b8d8
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-09-12 17:14:31 +0100

    Update splash_body.dart

--- lib/splash_screen/splash_body.dart
+++ lib/splash_screen/splash_body.dart
@@ -13,18 +13,51 @@ class Splash_Body extends StatelessWidget {
       children: [
         bkgImage,
         Container(
-          child: Column(
-            children: [
-              Column(
-                children: [
-                  Center(
-                    child: Image.asset(
-                      'assets/icons/app_icon_black_25.png',
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 120),
+            child: Column(
+              children: [
+                Center(
+                  child: Column(
+                    children: [
+                      Image.asset(
+                        'assets/icons/app_icon_black_25.png',
+                      ),
+                      Text(
+                        'PARADISE',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w500,
+                          letterSpacing: 1.0,
+                        ),
+                      ),
+                      Text(
+                        'HOME',
+                        style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500,
+                            letterSpacing: 3.90),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 70),
+                Column(
+                  children: [
+                    Text(
+                      'MAKE YOUR',
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
                     ),
-                  )
-                ],
-              )
-            ],
+                    Text(
+                      'HOME BEAUTIFUL',
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         )
       ],
