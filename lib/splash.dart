b52c28e55781f207c9a5a47671b76871c23fb091
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-09-12 15:48:21 +0100

    Updating main with splash screen

---
+++ lib/splash.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Splash_Screen extends StatelessWidget {
+  const Splash_Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
