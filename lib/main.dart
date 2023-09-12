911db1c5491e14a6c841830407658e96800e8ab4
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-09-12 17:44:31 +0100

    Creating home page mock ui

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,6 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
-      
       ),
       home: const Splash_Screen(),
     );
---
+++ lib/screens/home/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Text('Welcome home'),
+    ));
+  }
+}
