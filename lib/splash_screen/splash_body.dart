41ad7924fa0bde0fe35eaeda5e1ef2a4af7054e9
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-09-12 16:59:58 +0100

    Updating splash screen body with contents

--- lib/splash_screen/splash_body.dart
+++ lib/splash_screen/splash_body.dart
@@ -12,32 +12,18 @@ class Splash_Body extends StatelessWidget {
     return Stack(
       children: [
         bkgImage,
-        const Positioned(
-          top: 50,
+        Container(
           child: Column(
             children: [
-              Positioned(
-                left: 52,
-                top: 233,
-                child: Text(
-                  'MAKE YOUR ',
-                  style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w300,
-                      color: Colors.white),
-                ),
-              ),
-              Positioned(
-                left: 52,
-                top: 267,
-                child: Text(
-                  'HOME BEAUTIFUL',
-                  style: TextStyle(
-                      fontSize: 30,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600),
-                ),
-              ),
+              Column(
+                children: [
+                  Center(
+                    child: Image.asset(
+                      'assets/icons/app_icon_black_25.png',
+                    ),
+                  )
+                ],
+              )
             ],
           ),
         )
