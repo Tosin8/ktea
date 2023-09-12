52f999eddd43e18f3bd0ca61373c663804a0491b
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-09-12 21:32:17 +0100

    Update home_body.dart

---
+++ lib/screens/home/home_body.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Home_Body extends StatefulWidget {
+  const Home_Body({super.key});
+
+  @override
+  State<Home_Body> createState() => _Home_BodyState();
+}
+
+class _Home_BodyState extends State<Home_Body> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
