26922dba0e920b8c1786076928043b224d5e9c12
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-09-13 22:22:31 +0100

    Update home_widget.dart

--- lib/screens/home/home_widget.dart
+++ lib/screens/home/home_widget.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 
 import 'package:furniture_app/splash_screen/splash_widgets.dart';
 
+// ignore: camel_case_types
 class App_Drawer extends StatelessWidget {
   const App_Drawer({
     super.key,
@@ -12,21 +13,53 @@ class App_Drawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
         child: ListView(
-      children: [
-        const Padding(
-          padding: EdgeInsets.symmetric(vertical: 20),
+      children: const [
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 30),
           child: App_Icon(),
         ),
-        const Divider(),
-        Column(
-          children: [
-            ListTile(
-              leading: SvgPicture.asset('assets/images/home_black.svg'),
-              title: const Text('Home'),
-            ),
-          ],
-        )
+        Divider(),
+        Screen_Nav()
       ],
     ));
+  }
+}
+
+// ignore: camel_case_types
+class Screen_Nav extends StatelessWidget {
+  const Screen_Nav({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        GestureDetector(
+          onTap: () {},
+          child: ListTile(
+            leading: SvgPicture.asset('assets/images/home_black.svg'),
+            title: const Text('Home'),
+            splashColor: Colors.blueAccent,
+          ),
+        ),
+        ListTile(
+          leading: SvgPicture.asset('assets/images/home_black.svg'),
+          title: const Text('Cart'),
+        ),
+        ListTile(
+          leading: SvgPicture.asset('assets/images/home_black.svg'),
+          title: const Text('Saved Items'),
+        ),
+        ListTile(
+          leading: SvgPicture.asset('assets/images/home_black.svg'),
+          title: const Text('Profile'),
+        ),
+        ListTile(
+          leading: SvgPicture.asset('assets/images/home_black.svg'),
+          title: const Text('Help'),
+        ),
+      ],
+    );
   }
 }
