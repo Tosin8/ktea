5227bd60bb7d84c0a701623975bae80fed662cc8
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-09-14 02:02:53 +0100

    done updating ui header

--- lib/screens/home/home_widget.dart
+++ lib/screens/home/home_widget.dart
@@ -14,56 +14,23 @@ class App_Drawer extends StatelessWidget {
         elevation: 20,
         child: ListView(
           children: [
-            DrawerHeader(
-              decoration: const BoxDecoration(
+            const DrawerHeader(
+              decoration: BoxDecoration(
                   image: DecorationImage(
                       image: AssetImage('assets/images/drawer/1.jpg'),
                       fit: BoxFit.cover)),
               child: Stack(
                 fit: StackFit.loose,
                 clipBehavior: Clip.none,
-                children: [
-                  Center(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 30),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Image.asset('assets/icons/app_icon_white_25.png'),
-                          const Text(
-                            'PARADISE',
-                            style: TextStyle(
-                                fontSize: 15,
-                                color: Colors.white,
-                                fontWeight: FontWeight.w500,
-                                letterSpacing: 1.0),
-                          ),
-                          const Text(
-                            'HOME',
-                            style: TextStyle(
-                                fontSize: 15,
-                                color: Colors.white,
-                                fontWeight: FontWeight.w500,
-                                letterSpacing: 3.90),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    top: 102,
-                    child: const CircleAvatar(
-                      radius: 30,
-                      backgroundImage:
-                          AssetImage('assets/images/drawer/model.jpg'),
-                    ),
-                  ),
-                ],
+                children: [App_Icon_White(), Model_User(), User_Details()],
               ),
             ),
             const Screen_Nav(),
             const SizedBox(height: 220),
-            const Divider(),
+            const Divider(
+              color: Colors.white,
+              thickness: 1,
+            ),
             const SizedBox(height: 5),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -74,6 +41,107 @@ class App_Drawer extends StatelessWidget {
             ),
           ],
         ));
+  }
+}
+
+class User_Details extends StatelessWidget {
+  const User_Details({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: 115,
+      left: 70,
+      child: Row(
+        children: [
+          const Column(
+            children: [
+              Text(
+                'Abigael Landi',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 10),
+              ),
+              Text('abig@gmail.com',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 10,
+                      fontWeight: FontWeight.w500))
+            ],
+          ),
+          const SizedBox(width: 30),
+          Row(
+            children: [
+              Image.asset('assets/icons/location_10.png'),
+              const Text(
+                'Lagos, Nigeria',
+                style: TextStyle(
+                    fontSize: 10,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Model_User extends StatelessWidget {
+  const Model_User({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Positioned(
+      top: 102,
+      child: CircleAvatar(
+        radius: 30,
+        backgroundImage: AssetImage('assets/images/drawer/model.jpg'),
+      ),
+    );
+  }
+}
+
+class App_Icon_White extends StatelessWidget {
+  const App_Icon_White({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 30),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset('assets/icons/app_icon_white_25.png'),
+            const Text(
+              'PARADISE',
+              style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w500,
+                  letterSpacing: 1.0),
+            ),
+            const Text(
+              'HOME',
+              style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w500,
+                  letterSpacing: 3.90),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
