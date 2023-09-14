08f42c2ee9670eca6a2917ac6f36bda26d559657
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-09-14 17:15:27 +0100

    Creating individual widgets

--- lib/screens/categories/category_body.dart
+++ lib/screens/categories/category_body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'category_widget.dart';
+
 class Category_Body extends StatefulWidget {
   const Category_Body({super.key});
 
@@ -11,77 +13,11 @@ class _Category_BodyState extends State<Category_Body> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 30),
-      child: ListView(
-        children: [
-          Category_Menu(
-            bkgImage: 'assets/category_menu/home_deco_bkg.jpg',
-            productImg: 'assets/product_menu/home_deco_250.png',
-            titleText: 'HOME DECORATION',
-            press: () {},
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class Category_Menu extends StatelessWidget {
-  const Category_Menu({
-    super.key,
-    required this.bkgImage,
-    required this.productImg,
-    required this.titleText,
-    required this.press,
-  });
-
-  final String bkgImage, productImg;
-  final String titleText;
-  final VoidCallback press;
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      fit: StackFit.loose,
-      clipBehavior: Clip.none,
-      children: [
-        Container(
-            clipBehavior: Clip.antiAlias,
-            height: 190,
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
-            child: Image.asset(bkgImage)),
-        Positioned(top: -60, right: 180, child: Image.asset(productImg)),
-        Positioned(
-          top: 65,
-          left: 170,
-          child: Column(
-            children: [
-              Text(
-                titleText,
-                style: const TextStyle(
-                  fontWeight: FontWeight.w600,
-                  fontSize: 15,
-                ),
-              ),
-              const SizedBox(height: 10),
-              GestureDetector(
-                onTap: press,
-                child: Container(
-                  height: 30,
-                  width: 80,
-                  decoration:
-                      BoxDecoration(border: Border.all(color: Colors.black)),
-                  child: const Center(
-                    child: Text(
-                      'Shop Now',
-                    ),
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
-      ],
-    );
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        child: ListView(children: [
+          HomeDecoration(),
+          SizedBox(height: 20),
+          
+        ]));
   }
 }
--- lib/screens/categories/category_widget.dart
+++ lib/screens/categories/category_widget.dart
@@ -1,1 +1,57 @@
+import 'package:flutter/material.dart';
 
+class HomeDecoration extends StatelessWidget {
+  const HomeDecoration({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      fit: StackFit.loose,
+      clipBehavior: Clip.none,
+      children: [
+        Container(
+            clipBehavior: Clip.antiAlias,
+            height: 190,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
+            child: Image.asset('assets/category_menu/home_deco_bkg.jpg')),
+        Positioned(
+            top: -50,
+            right: 180,
+            child: Image.asset('assets/category_menu/home_deco_250.png')),
+        Positioned(
+          top: 65,
+          left: 170,
+          child: Column(
+            children: [
+              Text(
+                'HOME DECORATIONS',
+                style: const TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 15,
+                ),
+              ),
+              const SizedBox(height: 10),
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  height: 30,
+                  width: 80,
+                  decoration:
+                      BoxDecoration(border: Border.all(color: Colors.black)),
+                  child: const Center(
+                    child: Text(
+                      'Shop Now',
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
