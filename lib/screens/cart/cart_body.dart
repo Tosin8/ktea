de40256e914622e45c33d496f79f908c78edbd72
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-09-15 16:06:03 +0100

    Updating Cart UI

---
+++ lib/screens/cart/cart_body.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Cart_Body extends StatefulWidget {
+  const Cart_Body({super.key});
+
+  @override
+  State<Cart_Body> createState() => _Cart_BodyState();
+}
+
+class _Cart_BodyState extends State<Cart_Body> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
