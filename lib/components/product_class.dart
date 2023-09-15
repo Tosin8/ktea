3e15641849bd9fdd478e2090b27a4b16f4c82e7e
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-09-15 16:46:12 +0100

    Update product_class.dart

--- lib/components/product_class.dart
+++ lib/components/product_class.dart
@@ -19,48 +19,42 @@ List<Product_HomeDeco> products = [
     title: "Office Code",
     price: 234,
     description: dummyText,
-    image: "assets/images/bag_1.png",
+    image: "assets/products/home_decoration/1_150.jpg",
   ),
   Product_HomeDeco(
     id: 2,
     title: "Belt Bag",
     price: 234,
-    size: 8,
     description: dummyText,
-    image: "assets/images/bag_2.png",
+    image: "assets/images/1.png",
   ),
-  Product(
+  Product_HomeDeco(
     id: 3,
     title: "Hang Top",
     price: 234,
-    size: 10,
     description: dummyText,
     image: "assets/images/bag_3.png",
   ),
-  Product(
+  Product_HomeDeco(
     id: 4,
     title: "Old Fashion",
     price: 234,
-    size: 11,
     description: dummyText,
     image: "assets/images/bag_4.png",
   ),
-  Product(
-      id: 5,
-      title: "Office Code",
-      price: 234,
-      size: 12,
-      description: dummyText,
-      image: "assets/images/bag_5.png",
-      color: Color(0xFFFB7883)),
-  Product(
+  Product_HomeDeco(
+    id: 5,
+    title: "Office Code",
+    price: 234,
+    description: dummyText,
+    image: "assets/images/bag_5.png",
+  ),
+  Product_HomeDeco(
     id: 6,
     title: "Office Code",
     price: 234,
-    size: 12,
     description: dummyText,
     image: "assets/images/bag_6.png",
-    color: Color(0xFFAEAEAE),
   ),
 ];
 
