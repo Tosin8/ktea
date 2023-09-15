e0a4f7c04334aaa22ab97b653e2980bf248e505b
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-09-15 15:23:29 +0100

    Create product_class.dart

---
+++ lib/components/product_class.dart
@@ -0,0 +1,14 @@
+class Product {
+  final int price;
+  final String id, title, category, image, subTitle, description;
+
+  Product({
+    this.id,
+    this.price,
+    this.title,
+    this.category,
+    this.image,
+    this.subTitle,
+    this.description,
+  });
+}
