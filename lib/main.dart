bab117fc35a90454b01c8ba77f0a7efdb6007333
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-03 21:51:09 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:furniture_app/firebase_options.dart';
 import 'package:furniture_app/splash_screen/splash.dart';
 
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized(); 
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform, 
+  );
   runApp(const MyApp());
 }
 
