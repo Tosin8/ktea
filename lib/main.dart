c4ef92e61aea13f6f7988c20c70baf77e325c109
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-03 23:37:26 +0100

    Updating Pub with GetX state mgt

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:furniture_app/firebase_options.dart';
-import 'package:furniture_app/splash_screen/splash.dart';
+
+import 'firebase_options.dart';
+import 'splash_screen/splash.dart';
+
 
 
 Future<void> main() async {
