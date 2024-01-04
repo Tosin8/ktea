83674a1b48bbe6b3e5dffaf2e29101acbe16fe0c
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-04 13:11:10 +0100

    Update auth.dart

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -2,6 +2,7 @@
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:ktea/model/loginuser.dart';
 
 import '../model/firebaseuser.dart';
 
@@ -22,12 +23,43 @@ class AuthService {
 // Creating Sign In Anonmymous method. 
 Future signInAnonymous() async {
   try {
-    UserCredential userCredential = await _auth.signInAnonmyously();
+    UserCredential userCredential = await _auth.signInAnonymously();
     User? user = userCredential.user; 
     return _firebaseUser(user); 
   }
   catch (e){
     return FirebaseUser(code: e.toString(), uid: null); 
+  }
+}
+Future signInEmailPassword(LoginUser _login) async {
+  try {
+    UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(email: _login.email.toString(),
+     password: _login.password.toString()); 
+     User? user = userCredential.user; 
+     return _firebaseUser(user); 
+  } on FirebaseAuthException catch(e) {
+    return FirebaseUser(uid: null, code: e.code);
+  }
+}
+
+Future registerEmailPassword(LoginUser _login) async {
+  try {
+    UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: _login.email.toString(), password: _login.password.toString());
+    User? user = userCredential.user; 
+    return _firebaseUser(user);
+  } on FirebaseAuthException catch (e) {
+    return FirebaseUser(uid: null, code: e.code);
+  } catch (e) {
+    return FirebaseUser(uid: null, code: e.toString());
+  }
+}
+
+Future signOut() async {
+  try {
+    return await _auth.signOut();
+  }
+  catch (e) {
+    return null; 
   }
 }
 }
