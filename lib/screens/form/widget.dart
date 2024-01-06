3e81d4956813a67c2685eca44eee7ee69a97241a
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-06 20:18:24 +0100

    Update widget.dart

--- lib/screens/form/widget.dart
+++ lib/screens/form/widget.dart
@@ -11,27 +11,35 @@ class _AppFormState extends State<AppForm> {
   @override
   Widget build(BuildContext context) {
     return Form(
-      child: Column(
-        children: [
-          TextFormField(
-            decoration: InputDecoration(
-              labelText: 'Email', 
-              hintText: 'Enter your email', 
-              floatingLabelBehavior: FloatingLabelBehavior.always, 
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(28),
-                gapPadding: 10, 
-                borderSide: BorderSide(color: Colors.orange),
-              ), 
-              focusedBorder: OutlineInputBorder(
-                
-                borderRadius: BorderRadius.circular(28), 
-                borderSide: BorderSide(color: Colors.black), 
-                gapPadding: 10, 
-              )
-            ),
-          )
-        ],
+      child: Padding(
+        padding: const EdgeInsets.only(left: 15, right: 15),
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                suffixIcon: Padding(
+                  padding: const EdgeInsets.only(right: 10), 
+                  child: Icon(Icons.mail,)),
+
+                labelText: 'Email', 
+                hintText: 'Enter your email', hintStyle: TextStyle(color: Colors.grey[500]), 
+                contentPadding: const EdgeInsets.symmetric(horizontal: 42, vertical: 20),
+                floatingLabelBehavior: FloatingLabelBehavior.always, 
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(28),
+                  gapPadding: 10, 
+                  borderSide: const BorderSide(color: Colors.orange),
+                ), 
+                focusedBorder: OutlineInputBorder(
+                  
+                  borderRadius: BorderRadius.circular(28), 
+                  borderSide: const BorderSide(color: Colors.black), 
+                  gapPadding: 10, 
+                )
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
