c4f9f501fd7ed522a79560c0fe4927908e847808
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-16 14:41:47 +0100

    Update body.dart
    Updating with Form animation

--- lib/screens/form/signin/body.dart
+++ lib/screens/form/signin/body.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ktea/components/constants.dart';
@@ -16,31 +17,34 @@ class SignInBody extends StatelessWidget {
       child: SizedBox(
         width: double.infinity, 
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              //SizedBox(height: 8), 
-              const Text('Welcome Back',
-              style: TextStyle(color: Colors.black, fontSize: 28, fontWeight: FontWeight.bold),),
-              const SizedBox(height: 6),  
-              const Text('Sign in with your email and password \nor continue with your social media account',style: TextStyle(fontSize: 16), 
-              textAlign: TextAlign.center,), 
-              const SizedBox(height: 30), 
-              const AppForm(),
+          child: FadeInRight(
+            duration: Duration(milliseconds: 300),
+            child: Column(
+              children: [
+                //SizedBox(height: 8), 
+                const Text('Welcome Back',
+                style: TextStyle(color: Colors.black, fontSize: 28, fontWeight: FontWeight.bold),),
+                const SizedBox(height: 6),  
+                const Text('Sign in with your email and password \nor continue with your social media account',style: TextStyle(fontSize: 16), 
+                textAlign: TextAlign.center,), 
                 const SizedBox(height: 30), 
-                const Text('- OR USE - ') , 
-                const SizedBox(height: 8), 
-                const SocialCard(),
-                GestureDetector(
-                  onTap: (){
-                    Get.to(const SignUp());
-                  },
-                  child: const Text('Create Account', 
-                  style: TextStyle(
-                    color: Colors.orange,fontWeight: FontWeight.w600,
-                  fontSize: 18, decoration: TextDecoration.underline, decorationColor: Colors.orange)),
-                ) , 
-            ],
-            
+                const AppForm(),
+                  const SizedBox(height: 30), 
+                  const Text('- OR USE - ') , 
+                  const SizedBox(height: 8), 
+                  const SocialCard(),
+                  GestureDetector(
+                    onTap: (){
+                      Get.to(const SignUp());
+                    },
+                    child: const Text('Create Account', 
+                    style: TextStyle(
+                      color: Colors.orange,fontWeight: FontWeight.w600,
+                    fontSize: 18, decoration: TextDecoration.underline, decorationColor: Colors.orange)),
+                  ) , 
+              ],
+              
+            ),
           ),
         ),
       ),
