fd607ff7a6bac848cbca7b04d7a5a40755545ae2
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-16 18:55:11 +0100

    Update category_body.dart
    Adding Hero Widgets

--- lib/screens/categories/category_body.dart
+++ lib/screens/categories/category_body.dart
@@ -17,7 +17,7 @@ class _Category_BodyState extends State<Category_Body> {
         padding: const EdgeInsets.symmetric(vertical: 10,horizontal: 10
       ),
         child: ListView(
-          children:  const [
+          children:   [
             SizedBox(height: 10), 
           Text('Collection',
            style: TextStyle(color: Colors.black, fontSize: 30, fontWeight: FontWeight.w500),), 
@@ -35,7 +35,8 @@ class _Category_BodyState extends State<Category_Body> {
            maintitle: 'Modern Dining', 
            subtitle: 'Classic yet contemporary,\nwith a timeless quality.',
             mainTime: 500, 
-            delayTime: 0,
+            delayTime: 0, 
+            press: () {  }, herotag: '',
             ),
             SizedBox(height: 10), 
             CollectionsWidget(imagebkg: 'assets/images/collections/2.jpg', 
@@ -71,68 +72,78 @@ class _Category_BodyState extends State<Category_Body> {
 
 class CollectionsWidget extends StatelessWidget {
   const CollectionsWidget({
-    super.key, required this.imagebkg, required this.maintitle, required this.subtitle, required this.mainTime, required this.delayTime,
+    super.key, required this.imagebkg, required this.maintitle, required this.subtitle, required this.mainTime, required this.delayTime, required this.press, required this.herotag,
   });
 
-final String imagebkg; 
+final String imagebkg, herotag; 
 final String maintitle; 
 final String subtitle; 
 final int mainTime; 
 final int delayTime; 
+final GestureTapCallback press; 
+
   @override
   Widget build(BuildContext context) {
     return FadeInRight(
       duration: Duration(milliseconds: mainTime), 
       delay: Duration(milliseconds: delayTime),
-      child: Container(
-       height: 250,
-       width: 300,
-       decoration:  BoxDecoration(
-         borderRadius: BorderRadius.circular(10),
-         image:  DecorationImage(
-           image: AssetImage(imagebkg),
-           fit: BoxFit.cover,)
-       ),
-       child: Container(
-         decoration: BoxDecoration(
-           borderRadius: BorderRadius.circular(10),
-           gradient: LinearGradient(
-             colors: [
-               Colors.black.withOpacity(0.5),  
-               Colors.black.withOpacity(0.2), 
-               
-               
-               ], begin: Alignment.bottomRight )
-         ),
-         child: Padding(
-           padding: const EdgeInsets.all(10),
-           child: Column(
-             mainAxisAlignment: MainAxisAlignment.end,
-             crossAxisAlignment: CrossAxisAlignment.start,
-             children: [
-               Text(maintitle, 
-               style: const TextStyle(
-                 color: Colors.white, 
-               fontSize: 26,
-                fontWeight: FontWeight.w400),
-                ),
-                const SizedBox(height: 12), 
-                Row(
-                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      child: GestureDetector(
+        onTap: () {
+           press; 
+        },
+        child: Hero(
+          tag: herotag,
+          child: Container(
+           height: 250,
+           width: 300,
+           decoration:  BoxDecoration(
+             borderRadius: BorderRadius.circular(10),
+             image:  DecorationImage(
+               image: AssetImage(imagebkg),
+               fit: BoxFit.cover,)
+           ),
+           child: Container(
+             decoration: BoxDecoration(
+               borderRadius: BorderRadius.circular(10),
+               gradient: LinearGradient(
+                 colors: [
+                   Colors.black.withOpacity(0.5),  
+                   Colors.black.withOpacity(0.2), 
+                   
+                   
+                   ], begin: Alignment.bottomRight )
+             ),
+             child: Padding(
+               padding: const EdgeInsets.all(10),
+               child: Column(
+                 mainAxisAlignment: MainAxisAlignment.end,
+                 crossAxisAlignment: CrossAxisAlignment.start,
                  children: [
-                   Text(subtitle, 
+                   Text(maintitle, 
                    style: const TextStyle(
                      color: Colors.white, 
-                     fontSize: 15, fontWeight: FontWeight.w400),
-                     ),
-                     const Icon(Icons.arrow_forward_ios, color: Colors.white,)
+                   fontSize: 26,
+                    fontWeight: FontWeight.w400),
+                    ),
+                    const SizedBox(height: 12), 
+                    Row(
+                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                     children: [
+                       Text(subtitle, 
+                       style: const TextStyle(
+                         color: Colors.white, 
+                         fontSize: 15, fontWeight: FontWeight.w400),
+                         ),
+                         const Icon(Icons.arrow_forward_ios, color: Colors.white,)
+                     ],
+                    ), const SizedBox(height: 20,),  
+               
                  ],
-                ), const SizedBox(height: 20,),  
-           
-             ],
+               ),
+             ),
            ),
-         ),
-       ),
+          ),
+        ),
       ),
     );
   }
