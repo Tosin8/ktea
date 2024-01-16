f3a83d3a9bfc984c41d6031c255c5dd3e1f83462
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-16 14:33:34 +0100

    Refactoring Screen Pages

--- lib/screens/form/signin/body.dart
+++ lib/screens/form/signin/body.dart
@@ -10,7 +10,7 @@ class SignInBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    return const SafeArea(
       child: SizedBox(
         width: double.infinity, 
         child: SingleChildScrollView(
