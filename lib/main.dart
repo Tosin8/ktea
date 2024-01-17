7e987c757b0a5a58bc79e04bb0b58ec9702d44b0
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-17 16:00:23 +0100

    Refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:ktea/screens/form/signin/signin.dart';
 
 import 'firebase_options.dart';
 import 'splash_screen/splash.dart';
