53a4aa9f7e2d411b4e5774e4e4b3451ce0ae506d
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-17 16:08:53 +0100

    Update loginuser.dart

--- lib/model/loginuser.dart
+++ lib/model/loginuser.dart
@@ -1,7 +1,24 @@
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class LoginUser {
   final String? email; 
   final String? password;
 
   LoginUser({required this.email, required this.password}); 
   
+}
+
+class UserModel {
+  static const NAME = 'name'; 
+  static const EMAIL = 'email'; 
+
+  late String name, email; 
+
+  UserModel({required this.name, required this.email}); 
+  
+  UserModel.fromSnapshot(DocumentSnapshot snapshot) {
+    name = snapshot.data()[NAME]; 
+    email = snapshot.data()[EMAIL]; 
+  }
 }
