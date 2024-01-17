79c0cd12ae059093928a9254ac7145898b80ec88
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-17 16:21:52 +0100

    Update app_controller.dart

--- lib/controllers/app_controller.dart
+++ lib/controllers/app_controller.dart
@@ -1,10 +1,22 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:ktea/model/device.dart';
 
 class AppController extends GetxController {
   static AppController instance = Get.find(); 
+  Rx<Widget> activeDraggableWidget = Rx<Widget>(DevicesDraggable()); 
+  Rx<DeviceModel> activeDevice = DeviceModel().obs; 
   RxBool isLoginWidgetDisplayed = true.obs; 
 
   changeDisplayAuthWidget() {
     isLoginWidgetDisplayed.value = !isLoginWidgetDisplayed.value; 
+  }
+
+  changeActiveDeviceTo(DeviceModel device) {
+    activeDevice.value = device; 
+  }
+
+  changeActiveDraggableWidgetTo(Widget widget) {
+    activeDraggableWidget.value = widget; 
   }
 }
