24751cda6ded069963696f3182a51ed7c08d4e63
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-18 14:03:35 +0100

    Update app_controller.dart

--- lib/controllers/app_controller.dart
+++ lib/controllers/app_controller.dart
@@ -1,22 +1,22 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:ktea/model/device.dart';
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import 'package:ktea/model/device.dart';
 
-class AppController extends GetxController {
-  static AppController instance = Get.find(); 
-  Rx<Widget> activeDraggableWidget = Rx<Widget>(DevicesDraggable()); 
-  Rx<DeviceModel> activeDevice = DeviceModel().obs; 
-  RxBool isLoginWidgetDisplayed = true.obs; 
+// class AppController extends GetxController {
+//   static AppController instance = Get.find(); 
+//   Rx<Widget> activeDraggableWidget = Rx<Widget>(DevicesDraggable()); 
+//   Rx<DeviceModel> activeDevice = DeviceModel().obs; 
+//   RxBool isLoginWidgetDisplayed = true.obs; 
 
-  changeDisplayAuthWidget() {
-    isLoginWidgetDisplayed.value = !isLoginWidgetDisplayed.value; 
-  }
+//   changeDisplayAuthWidget() {
+//     isLoginWidgetDisplayed.value = !isLoginWidgetDisplayed.value; 
+//   }
 
-  changeActiveDeviceTo(DeviceModel device) {
-    activeDevice.value = device; 
-  }
+//   changeActiveDeviceTo(DeviceModel device) {
+//     activeDevice.value = device; 
+//   }
 
-  changeActiveDraggableWidgetTo(Widget widget) {
-    activeDraggableWidget.value = widget; 
-  }
-}
+//   changeActiveDraggableWidgetTo(Widget widget) {
+//     activeDraggableWidget.value = widget; 
+//   }
+// }
