7c84c42752c2a82a376cccb16705e36c8ddcab43
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-22 23:07:21 +0100

    adding

--- lib/screens/form/forgot_pass/forgot_body.dart
+++ lib/screens/form/forgot_pass/forgot_body.dart
@@ -1,4 +1,8 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:ktea/screens/form/widget.dart';
+
+import 'forgot_pass_widget.dart';
 
 class ForgotPwdBody extends StatefulWidget {
   const ForgotPwdBody({super.key});
@@ -10,6 +14,20 @@ class ForgotPwdBody extends StatefulWidget {
 class _ForgotPwdBodyState extends State<ForgotPwdBody> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SizedBox( 
+      width: double.infinity, 
+      child: SingleChildScrollView(child: FadeInRight(child: Column(children: [
+        SizedBox(height: 8,), 
+       Text('Reset your password',
+                style: TextStyle(color: Colors.black.withOpacity(0.8), fontSize: 24, fontWeight: FontWeight.bold),),
+                const SizedBox(height: 6),  
+                const Text('Type in your registered email\n to receive reset code',style: TextStyle(fontSize: 16), 
+                textAlign: TextAlign.center,), 
+                const SizedBox(height: 30), 
+                const ForgotPwdForm(), 
+                const SizedBox(height: 30), 
+                DefaultButton(textBtn: 'Reset Password', press: (){})
+      ],)),),
+    );
   }
 }
---
+++ lib/screens/form/forgot_pass/forgot_pass_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ForgotPwdForm extends StatefulWidget {
+  const ForgotPwdForm({super.key});
+
+  @override
+  State<ForgotPwdForm> createState() => _ForgotPwdFormState();
+}
+
+class _ForgotPwdFormState extends State<ForgotPwdForm> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
