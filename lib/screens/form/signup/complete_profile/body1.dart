bc7b6d4d08956f8241239e2630458a0a63823805
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-22 20:59:56 +0100

    Creating complete form form

--- lib/screens/form/signup/complete_profile/body1.dart
+++ lib/screens/form/signup/complete_profile/body1.dart
@@ -1,4 +1,7 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+
+import 'complete_profile_widget.dart';
 
 class CompleteProfileBody extends StatefulWidget {
   const CompleteProfileBody({super.key});
@@ -10,6 +13,17 @@ class CompleteProfileBody extends StatefulWidget {
 class _CompleteProfileBodyState extends State<CompleteProfileBody> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SizedBox( 
+      width: double.infinity,
+      child: SingleChildScrollView(
+        child: FadeInRight(child: Column(children: [
+          Text('Complete Profile', style: TextStyle(color: Colors.black.withOpacity(0.8), fontSize: 24, fontWeight: FontWeight.bold),), 
+          const SizedBox(height: 6,), 
+          Text('Complete your details or continue \nwith social media', textAlign: TextAlign.center,style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),), 
+          SizedBox(height: 50), 
+          const AppForm2(), 
+        ],)),
+      ),
+    );
   }
 }
---
+++ lib/screens/form/signup/complete_profile/complete_profile_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppForm2 extends StatefulWidget {
+  const AppForm2({super.key});
+
+  @override
+  State<AppForm2> createState() => _AppForm2State();
+}
+
+class _AppForm2State extends State<AppForm2> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
