7d5309fd2960d8d4b70193b43e8970f95180a148
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-22 21:58:01 +0100

    Update otp_body.dart

--- lib/screens/form/otp/otp_body.dart
+++ lib/screens/form/otp/otp_body.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 
 class OTPBody extends StatefulWidget {
@@ -10,6 +11,24 @@ class OTPBody extends StatefulWidget {
 class _OTPBodyState extends State<OTPBody> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SizedBox( 
+      width: double.infinity,
+      child: SingleChildScrollView(
+        child: FadeInRight(child: Column(children: [
+          SizedBox(height: 20), 
+          Text('OTP Verification', style: TextStyle(color: Colors.black.withOpacity(0.8), fontSize: 24, fontWeight: FontWeight.bold),
+        ), 
+        const SizedBox(height: 6,), 
+        const Text("We've sent your code to your phone number "), 
+        SizedBox(height: 10), 
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('This code will expire in '),
+          ],
+        )
+        ],)),
+      ),
+    );
   }
 }
