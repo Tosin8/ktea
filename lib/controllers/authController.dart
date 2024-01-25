a46f0fc705830508fd278efe504422822df24d96
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-25 16:49:45 +0100

    Update authController.dart

--- lib/controllers/authController.dart
+++ lib/controllers/authController.dart
@@ -1,5 +1,27 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class UserController extends GetxController {
   static UserController instance = Get.find(); 
+  Rx<User> firebaseUser; 
+  RxBool isLoggedIn = false.obs; 
+  TextEditingController name = TextEditingController(); 
+  TextEditingController email = TextEditingController(); 
+  TextEditingController password = TextEditingController(); 
+  String usersCollection = 'users'; 
+  Rx<UserModel> userModel = UserModel().obs; 
+
+  @override 
+  void onReady() {
+    super.onReady(); 
+    firebaseUser = Rx<User>(auth.currentUser); 
+    firebaseUser.bindStream(auth.userChanges()); 
+    ever(firebaseUser, _setInitialScreen);
+  }
+
+  _setInitialScreen(User user) {
+    if(user == null) {
+      Get.offAll(page);
+    }
+  }
 }
