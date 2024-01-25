f86c6124b9acb7649fbbcf95ff6e878cd87f9309
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-25 15:48:35 +0100

    Update dining_body.dart

--- lib/screens/categories/dining/dining_body.dart
+++ lib/screens/categories/dining/dining_body.dart
@@ -4,6 +4,8 @@ import 'package:get/get.dart';
 import 'dining_body_widget.dart';
 
 
+
+
 // class _DiningScreenState extends State<DiningScreen> {
 //   @override
 //   Widget build(BuildContext context) {
@@ -23,6 +25,7 @@ import 'dining_body_widget.dart';
 //   }
 // }
 
+
 class DiningScreen extends StatefulWidget {
   const DiningScreen({super.key});
 
@@ -33,49 +36,6 @@ class DiningScreen extends StatefulWidget {
 class _DiningScreenState extends State<DiningScreen> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold( 
-      body: CustomScrollView(slivers: [
-        SliverAppBar(
-         
-          expandedHeight: 220, 
-          flexibleSpace: FlexibleSpaceBar(
-            background: Image.asset('assets/images/collections/1.jpg', 
-            fit: BoxFit.cover,),
-            
-          ),
-          leading:  Padding(padding: EdgeInsets.only(left: 18), child: IconButton(onPressed: (){
-            Get.back();
-
-          }, icon: Icon(Icons.arrow_back_ios, color: Colors.white,)),
-          ),
-          actions: [
-            IconButton(onPressed: (){}, icon: const Icon(Icons.search_rounded, color: Colors.white,)), 
-             IconButton(onPressed: (){}, icon: const Icon(Icons.notifications, color: Colors.white,))
-          ],
-          
-        ), 
-        SliverToBoxAdapter(
-          child: DiningInfo(),
-        )
-      ],)
-    );
-  }
-}
-
-
-class DiningInfo extends StatefulWidget {
-  const DiningInfo({super.key});
-
-  @override
-  State<DiningInfo> createState() => _DiningInfoState();
-}
-
-class _DiningInfoState extends State<DiningInfo> {
-  @override
-  Widget build(BuildContext context) {
     return const Placeholder();
   }
 }
-
-
-     
