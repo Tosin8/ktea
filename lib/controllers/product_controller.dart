72590a6e9f3015aa7dfc9388343eb3df52cc5931
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-25 17:10:44 +0100

    Creating product model

--- lib/controllers/product_controller.dart
+++ lib/controllers/product_controller.dart
@@ -1,5 +1,20 @@
 import 'package:get/get.dart';
+import 'package:ktea/constants/firebase.dart';
 
 class ProductsController extends GetxController{
   static ProductsController instance = Get.find(); 
+  RxList<ProductModel> products = RxList<ProductModel>([]); 
+  String collection = 'products_dining'; 
+
+  @override 
+  void onReady() {
+    
+    super.onReady();
+    products_dining.bindStream(getAllProducts());
+  }
+
+  Stream<List<ProductModel>> getAllProducts() => 
+  firebaseFirestore.collection(collection).snapshots().map((query) => 
+  query.docs.map((item) => ProductModel.fromMap(item.data()))
+  .toList()); 
 }
---
+++ lib/model/product.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class ProductModel {
+  static const ID = 'id'; 
+  static const IMAGE = 'image'; 
+  static const NAME = 'name'; 
+  static const PRICE = 'price'; 
+
+  String id;
+  String image; 
+  String name; 
+  double price;
+  ProductModel({
+    required this.id,
+    required this.image,
+    required this.name,
+    required this.price,
+  });
+}
+
+ProductModel.fromMap(Map<String, dynamic> data ) {
+  id = data[ID]; 
+  image = data[IMAGE]; 
+  name = data[NAME]; 
+  price = data[PRICE].toDouble(); 
+}
