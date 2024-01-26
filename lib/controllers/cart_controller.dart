403df24e18997b05556a8b6a4eda92f67e06d532
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-26 23:04:00 +0100

    Update cart_controller.dart

--- lib/controllers/cart_controller.dart
+++ lib/controllers/cart_controller.dart
@@ -1,5 +1,24 @@
 import 'package:get/get.dart';
 
 class CartController extends GetxController{
-  
+  static CartController instance = Get.find(); 
+  RxDouble totalCartPrice = 0.0.obs; 
+
+  @override 
+  void onReady() {
+    super.onReady(); 
+    ever(userController.userModel,
+     changeCartTotalPrice); 
+  }
+
+  void addProductToCart(ProductModel product) {
+    try {
+      if (_isItemAlreadyAdded(product)) {
+        Get.snackbar('Check your cart', '${product.name} is already added');
+      } else {
+        String itemId = Uuid().toString(); 
+        userController.updateUserData
+      }
+    }
+  }
 }
