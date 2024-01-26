91e098c9b3d115452069a9b654803b6b80161be2
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-27 00:52:14 +0100

    Update cart_item.dart

--- lib/model/cart_item.dart
+++ lib/model/cart_item.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 class CartItemModel {
   // ignore: constant_identifier_names
   static const ID = 'id'; 
@@ -16,4 +17,33 @@ class CartItemModel {
   double cost;
   double price;
   String productId;
+  CartItemModel({
+    required this.id,
+    required this.image,
+    required this.name,
+    required this.quantity,
+    required this.cost,
+    required this.price,
+    required this.productId,
+  });
+
+//  static fromMap(Map<String, dynamic> data) {}
+CartItemModel.fromMap(Map<String, dynamic> data) {
+  id = data[ID]; 
+    image = data[IMAGE];
+    name = data[NAME];
+    quantity = data[QUANTITY];
+    cost = data[COST].toDouble();
+    productId = data[PRODUCT_ID]; 
+    price = data[PRICE].toDouble();  
+}
+
+Map toJson() => {
+  ID: id,
+  IMAGE: image,
+  NAME: name,
+  QUANTITY: quantity,
+  COST: price * quantity, 
+  PRICE: price,
+}; 
 }
