6a66641b5a045f480d0a5bede933c965d682fd1f
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-27 00:47:33 +0100

    Update cart_item.dart

--- lib/model/cart_item.dart
+++ lib/model/cart_item.dart
@@ -1,5 +1,19 @@
 class CartItemModel {
+  // ignore: constant_identifier_names
   static const ID = 'id'; 
-   static const IMAGE = 'id';
-    static const ID = 'id'; static const ID = 'id'; static const ID = 'id'; static const ID = 'id';
+   static const IMAGE = 'image';
+    static const NAME = 'name'; 
+    static const QUANTITY = 'quantity'; 
+    static const COST = 'cost'; 
+    static const PRICE = 'price';
+     static const PRODUCT_ID = 'productId'; 
+
+
+  String id;
+  String image;
+  String name;
+  int quantity;
+  double cost;
+  double price;
+  String productId;
 }
