ea921e53679a4e66f26f3a6a9387c15f47d2dea0
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-27 01:11:46 +0100

    Update payments.dart

--- lib/model/payments.dart
+++ lib/model/payments.dart
@@ -1,9 +1,27 @@
 class PaymentsModel{
   static const ID = 'id'; 
-   static const PAYMENT_ID = 'id'; 
-    static const CART = 'id'; 
-     static const AMOUNT = 'id';  
-     static const STATUS = 'id'; 
-      static const CREATED_AT = 'id';  
+   static const PAYMENT_ID = 'paymentId'; 
+    static const CART = 'cart'; 
+     static const AMOUNT = 'amount';  
+     static const STATUS = 'status'; 
+      static const CREATED_AT = 'createdAt';  
+
+
+      late String id; 
+      late String paymentId; 
+     late String status; 
+      late String amount; 
+      late String createdAt; 
+      late List cart; 
+      PaymentsModel({required this.id, required this.paymentId, required this.status, required this.amount, required this.createdAt, required this.cart});
+
+      PaymentsModel.fromMap(Map data){
+        id = data[ID];
+        paymentId = data[PAYMENT_ID];
+        status = data[STATUS];
+        amount = data[AMOUNT];
+        createdAt = data[CREATED_AT];
+        cart = data[CART];
+      }
      
 }
