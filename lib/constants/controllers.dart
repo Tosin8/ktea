bddf5ad454b877ca6d122ef23a63e4c9b1716f10
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-27 01:15:06 +0100

    Create controllers.dart

---
+++ lib/constants/controllers.dart
@@ -0,0 +1,7 @@
+import 'package:ktea/controllers/product_controller.dart';
+
+AppController appController = AppController.instance; 
+UserController userController = UserController.instance; 
+ProductsController productsController = ProductsController.instance; 
+CartController cartController = CartController.instance;
+PaymentsController paymentsController = PaymentsController.instance; 
