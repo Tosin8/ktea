d69ebd88fdb72fd843a58013e426498bc64ef9aa
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-27 01:07:04 +0100

    Create payments

---
+++ lib/model/payments.dart
@@ -0,0 +1,9 @@
+class PaymentsModel{
+  static const ID = 'id'; 
+   static const ID = 'id'; 
+    static const ID = 'id'; 
+     static const ID = 'id';  
+     static const ID = 'id'; 
+      static const ID = 'id';  
+      static const ID = 'id'; 
+}
