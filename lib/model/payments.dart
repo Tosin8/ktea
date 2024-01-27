c15c51e3a298ae142ed13a8f2683c55fc1538409
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-27 01:07:53 +0100

    Update payments.dart

--- lib/model/payments.dart
+++ lib/model/payments.dart
@@ -1,9 +1,9 @@
 class PaymentsModel{
   static const ID = 'id'; 
-   static const ID = 'id'; 
-    static const ID = 'id'; 
-     static const ID = 'id';  
-     static const ID = 'id'; 
-      static const ID = 'id';  
-      static const ID = 'id'; 
+   static const PAYMENT_ID = 'id'; 
+    static const CART = 'id'; 
+     static const AMOUNT = 'id';  
+     static const STATUS = 'id'; 
+      static const CREATED_AT = 'id';  
+     
 }
