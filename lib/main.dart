0309a4fc8a1ef2fbe486b4b51b7ecaffa6885277
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-27 01:59:22 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:ktea/constants/controllers.dart';
 import 'package:ktea/constants/firebase.dart';
 import 'package:ktea/controllers/app_controller.dart';
+import 'package:ktea/controllers/cart_controller.dart';
+import 'package:ktea/controllers/payments_controller.dart';
+import 'package:ktea/controllers/product_controller.dart';
 
+import 'controllers/authController.dart';
 import 'firebase_options.dart';
 import 'splash_screen/splash.dart';
 
@@ -16,7 +21,12 @@ Future<void> main() async {
   //   options: DefaultFirebaseOptions.currentPlatform, 
   // );
 
-  await initialization.then((value) => Get.put(AppController())); 
+  await initialization.then((value) => 
+  Get.put(AppController())); 
+  Get.put(UserController()); 
+  Get.put(ProductsController()); 
+  Get.put(CartController()); 
+  Get.put(PaymentsController()); 
     
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
