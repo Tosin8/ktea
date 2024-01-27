15f0276a51f1c69f2a8c47be5ef61f9558432982
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-27 01:46:34 +0100

    Creating paymentscreen

--- lib/constants/controllers.dart
+++ lib/constants/controllers.dart
@@ -1,5 +1,10 @@
 import 'package:ktea/controllers/product_controller.dart';
 
+import '../controllers/app_controller.dart';
+import '../controllers/authController.dart';
+import '../controllers/cart_controller.dart';
+import '../controllers/payments_controller.dart';
+
 AppController appController = AppController.instance; 
 UserController userController = UserController.instance; 
 ProductsController productsController = ProductsController.instance; 
---
+++ lib/controllers/payments_controller.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:ktea/utils/helpers/showLoading.dart';
+import 'package:uuid/uuid.dart';
+
+import 'package:stripe_payment/stripe_payment.dart';
+
+import '../constants/app_constants.dart';
+import '../constants/controllers.dart';
+import '../constants/firebase.dart';
+import '../model/payments.dart';
+
+class PaymentsController extends GetxController {
+  static PaymentsController instance = Get.find();
+  String collection = "payments";
+  String url =
+      'https://us-central1-sneex-cbc6a.cloudfunctions.net/createPaymentIntent';
+  List<PaymentsModel> payments = [];
+
+  @override
+  void onReady() {
+    super.onReady();
+    StripePayment.setOptions(StripeOptions(
+        publishableKey: "pk_test_vdof9EwWNjWX6gglW2xjji3Y00mLAezzRK",
+        androidPayMode: 'test'));
+  }
+
+
+
+
+  Future<void> createPaymentMethod() async {
+    StripePayment.setStripeAccount(null);
+    //step 1: add card
+    PaymentMethod paymentMethod = PaymentMethod();
+    paymentMethod = await StripePayment.paymentRequestWithCardForm(
+      CardFormPaymentRequest(),
+    ).then((PaymentMethod paymentMethod) {
+      return paymentMethod;
+    }).catchError((e) {
+      print('Error Card: ${e.toString()}');
+    });
+    paymentMethod != null
+        ? processPaymentAsDirectCharge(paymentMethod)
+        : _showPaymentFailedAlert();
+    dismissLoadingWidget();
+  }
+
+  Future<void> processPaymentAsDirectCharge(PaymentMethod paymentMethod) async {
+    showLoading();
+
+    int amount = (double.parse(cartController.totalCartPrice.value.toStringAsFixed(2)) * 100).toInt();
+    //step 2: request to create PaymentIntent, attempt to confirm the payment & return PaymentIntent
+    final response = await Dio()
+        .post('$url?amount=$amount&currency=USD&pm_id=${paymentMethod.id}');
+    print('Now i decode');
+    if (response.data != null && response.data != 'error') {
+      final paymentIntentX = response.data;
+      final status = paymentIntentX['paymentIntent']['status'];
+      if (status == 'succeeded') {
+        StripePayment.completeNativePayRequest();
+        _addToCollection(paymentStatus: status, paymentId: paymentMethod.id);
+        userController.updateUserData({"cart": []});
+        Get.snackbar("Success", "Payment succeeded");
+      }else{
+        _addToCollection(paymentStatus: status, paymentId: paymentMethod.id);
+      }
+    } else {
+      //case A
+      StripePayment.cancelNativePayRequest();
+      _showPaymentFailedAlert();
+    }
+  }
+
+  void _showPaymentFailedAlert() {
+    Get.defaultDialog(
+        content: CustomText(
+          text: "Payment failed, try another card",
+        ),
+        actions: [
+          GestureDetector(
+              onTap: () {
+                Get.back();
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: CustomText(
+                  text: "Okay",
+                ),
+              ))
+        ]);
+  }
+
+  _addToCollection({required String paymentStatus, required String paymentId}){
+    String id = Uuid().v1();
+    firebaseFirestore.collection(collection).doc(id).set({
+      "id": id,
+      "clientId": userController.userModel.value.id,
+      "status": paymentStatus,
+      "paymentId": paymentId,
+      "cart": userController.userModel.value.cartItemsToJson(),
+      "amount": cartController.totalCartPrice.value.toStringAsFixed(2),
+      "createdAt": DateTime.now().microsecondsSinceEpoch,
+    });
+  }
+
+  getPaymentHistory() {
+    showLoading();
+    payments.clear();
+    firebaseFirestore
+        .collection(collection)
+        .where("clientId", isEqualTo: userController.userModel.value.id)
+        .get()
+        .then((snapshot) {
+      snapshot.docs.forEach((doc) {
+        PaymentsModel payment = PaymentsModel.fromMap(doc.data());
+        payments.add(payment);
+      });
+
+      logger.i("length ${payments.length}");
+      dismissLoadingWidget();
+      Get.to(() => PaymentsScreen());
+    });
+  }
+}
