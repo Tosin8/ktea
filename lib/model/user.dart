aadb612958e9964b16dcce8e8c501e8969a75547
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-27 01:05:05 +0100

    Update user.dart

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -9,18 +9,18 @@ class UserModel {
   static const EMAIL = "email";
   static const CART = "cart";
 
-   String id;
-   String name;
-   String email;
-   List<CartItemModel> cart;
+   late String id;
+   late String name;
+   late String email;
+  late  List<CartItemModel> cart;
 
   UserModel({required this.id, required this.name, required this.email, required this.cart});
 
   UserModel.fromSnapshot(DocumentSnapshot snapshot) {
-    name = snapshot.data()[NAME];
-    email = snapshot.data()[EMAIL];
-    id = snapshot.data()[ID];
-    cart = _convertCartItems(snapshot.data()[CART] ?? []);
+    name = (snapshot.data() as Map<String, dynamic>)[NAME];
+    email = (snapshot.data() as Map<String, dynamic>)[EMAIL];
+    id = (snapshot.data() as Map<String, dynamic>)[ID];
+    cart = _convertCartItems((snapshot.data() as Map<String, dynamic>)[CART] ?? []);
   }
 
   List<CartItemModel> _convertCartItems(List cartFomDb){
