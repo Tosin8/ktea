bd8ca2eadb339f15d3c98d4ea43bde5dcaf10556
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-02-21 17:08:43 +0100

    Update products.dart

--- lib/utils/products.dart
+++ lib/utils/products.dart
@@ -1,21 +1,21 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
 
-import '../constants/controllers.dart';
-import '../model/product.dart';
-import 'single_product.dart';
+// import '../constants/controllers.dart';
+// import '../model/product.dart';
+// import 'single_product.dart';
 
-class ProductsWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Obx(()=>GridView.count(
-        crossAxisCount: 2,
-        childAspectRatio: .63,
-        padding: const EdgeInsets.all(10),
-        mainAxisSpacing: 4.0,
-        crossAxisSpacing: 10,
-        children: productsController.products.map((ProductModel product) {
-          return SingleProductWidget(product: product, key: null,);
-        }).toList()));
-  }
-}
+// class ProductsWidget extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Obx(()=>GridView.count(
+//         crossAxisCount: 2,
+//         childAspectRatio: .63,
+//         padding: const EdgeInsets.all(10),
+//         mainAxisSpacing: 4.0,
+//         crossAxisSpacing: 10,
+//         children: productsController.products.map((ProductModel product) {
+//           return SingleProductWidget(product: product, key: null,);
+//         }).toList()));
+//   }
+// }
