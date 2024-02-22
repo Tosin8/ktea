55b2435cf9df7928a982eee7e3d912597d10a528
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-02-22 02:44:25 +0100

    Updating images for new arrival

--- lib/model/new_arrival.dart
+++ lib/model/new_arrival.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 class New {
+  String imageUrl; 
   String title; 
   String Category; 
   double Price; 
@@ -7,5 +8,27 @@ class New {
     required this.title,
     required this.Category,
     required this.Price,
+    required this.imageUrl
   });
+
+  List <New> news = [
+    New(
+      title:"4 Set Wooden Dining",
+       Category:"Furniture", 
+        Price: 2500, imageUrl: ''),
+       New(
+      title:"iPhone 13 Pro Max", 
+      Category:"Cabinet",  
+      Price: 3000, imageUrl: ''),   
+       New(
+      title:"iPhone 13 Pro Max",
+       Category:"Smartphones",
+         Price: 3000, imageUrl: ''),
+          New(
+      title:"iPhone 13 Pro Max",
+       Category:"Smartphones",
+         Price: 3000, imageUrl: ''),
+  ];
 }
+
+
