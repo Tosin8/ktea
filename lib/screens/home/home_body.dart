d7975645224aa1b134b7b5adb2d4085c63510a8a
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-02-23 00:53:06 +0100

    Update home_body.dart

--- lib/screens/home/home_body.dart
+++ lib/screens/home/home_body.dart
@@ -75,58 +75,75 @@ class _Home_BodyState extends State<Home_Body> {
 const SizedBox(height: 10),
 titleCategory(title: 'Popular', press: (){}),
 const SizedBox(height: 10), 
-Container(
-  height: 120, width: 300, 
-  decoration: BoxDecoration(
-    borderRadius: BorderRadius.circular(15),
-    boxShadow: const [
-      BoxShadow(
-        color: Colors.black12
-      ),
-    ]
-  ),
-  child: Row(
-    children: [
-      Padding(
-        padding: const EdgeInsets.only(left: 8.0),
-        child: Container(
-          height: 100, width: 100,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            image: DecorationImage(
-              image: AssetImage(
-                popularProducts[0].imageUrl, 
-              ), fit: BoxFit.cover, 
-            )
-          ),
-         )),
-         const SizedBox(width: 10),
-         Padding(
-           padding: const EdgeInsets.all(8.0),
-           child: Column(
-            children: [
-              Text(
-                popularProducts[0].title, 
-                style: const TextStyle(
-                  color: Colors.black, 
-                fontWeight: FontWeight.bold, fontSize: 18),), 
-                Row(
-                  children: [
-                    Text(popularProducts[0].category), 
-                    Text(popularProducts[0].price.toString()), 
-                  ],
-                )
-            ],
-           ),
-         )
-      
-    ],
-  ),
-)
+popularProductCard()
         
         ]
          )
          ); 
+  }
+}
+
+class popularProductCard extends StatelessWidget {
+  const popularProductCard({
+    super.key, required this.popularProducts,
+  });
+
+final Popular popularProducts; 
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 90, width: 300, 
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: Colors.white,
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black12, 
+            offset: Offset(1, 1)
+          ),
+        ]
+      ),
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Container(
+              height: 80, width: 80,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                image: DecorationImage(
+                  image: AssetImage(
+                    popularProducts[0].imageUrl, 
+                  ), fit: BoxFit.cover, 
+                )
+              ),
+             )),
+             const SizedBox(width: 10),
+             Padding(
+               padding: const EdgeInsets.all(10.0),
+               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    popularProducts[0].title, 
+                    style: const TextStyle(
+                      color: Colors.black, 
+                    fontWeight: FontWeight.bold, fontSize: 20),), 
+                    SizedBox(height: 6,), 
+                    Row(
+                      children: [
+                        Text(popularProducts[0].category, style: TextStyle(fontSize: 18, color: Colors.black.withOpacity(0.6)),), 
+                        Text(popularProducts[0].price.toString(), style: TextStyle(fontSize: 18, 
+                        fontWeight: FontWeight.w600),), 
+                      ],
+                    )
+                ],
+               ),
+             )
+          
+        ],
+      ),
+    );
   }
 }
 
