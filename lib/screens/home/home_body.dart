5662035ed270144672d2a885518d3e16c195df3d
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-02-22 17:23:44 +0100

    Update home_body.dart

--- lib/screens/home/home_body.dart
+++ lib/screens/home/home_body.dart
@@ -1,6 +1,7 @@
 import 'package:animate_do/animate_do.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 import 'package:ktea/components/app_widgets.dart';
 
@@ -53,56 +54,7 @@ class _Home_BodyState extends State<Home_Body> {
             press: (){},
             ), 
           const SizedBox(height: 10),
-      Container(
-        width: 200, 
-        height: 300,
-        child: Card(
-          elevation: 0,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-          
-                height: 200, width: 200,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  image: DecorationImage(
-                    image: AssetImage(
-                      newProducts[0].imageUrl,
-                      
-                    ),
-                     fit: BoxFit.cover)
-                ),
-              ), 
-              const SizedBox(height: 10),
-              Text(
-                newProducts[0].title,style: TextStyle(
-                  fontSize: 16, fontWeight: FontWeight.w600, color: Colors.black.withOpacity(0.7)),
-                ),
-              
-              Text(newProducts[0].category, 
-              style: const TextStyle(color: Colors.grey, fontSize: 15),) ,
-               
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(newProducts[0].price.toString(), 
-                  style: const TextStyle(
-                    color: Colors.black, fontSize: 16),
-                    ),
-                  const SizedBox(width: 10),
-                  GestureDetector(
-                    onTap: () {
-                      
-                    },
-                    child: Icon(Icons.add_circle, size: 30,),
-                  )
-                ],
-              )
-            ]
-          ),
-        ),
-      )
+      newArrivalProductCard()
         
 
           
@@ -115,4 +67,62 @@ class _Home_BodyState extends State<Home_Body> {
   }
 }
 
-//
+class newArrivalProductCard extends StatelessWidget {
+  const newArrivalProductCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+        borderRadius: BorderRadius.circular(5),
+      child: Card(
+        elevation: 1,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+        
+              height: 200, width: 200,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                image: DecorationImage(
+                  image: AssetImage(
+                    newProducts[0].imageUrl,
+                    
+                  ),
+                   fit: BoxFit.cover)
+              ),
+            ), 
+            const SizedBox(height: 10),
+            Text(
+              newProducts[0].title,style: TextStyle(
+                fontSize: 16, fontWeight: FontWeight.w600, color: Colors.black.withOpacity(0.7)),
+              ),
+            
+            Text(newProducts[0].category, 
+            style: const TextStyle(color: Colors.grey, fontSize: 15),) ,
+             
+            Row(
+             
+              children: [
+                Text(newProducts[0].price.toString(), 
+                style: const TextStyle(
+                  color: Colors.black, fontSize: 16),
+                  ),
+                const SizedBox(width: 120),
+                GestureDetector(
+                  onTap: () {
+                    
+                  },
+                  child: Icon(Icons.add_circle, size: 30,),
+                )
+              ],
+            )
+          ]
+        ),
+      ),
+    );
+  }
+}
+
