c17803c634d43e9a5279005d82984b72030f84ce
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-02-23 00:02:26 +0100

    Create popular.dart

---
+++ lib/model/popular.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Popular {
+  String imageUrl;
+  String title; 
+  String category;
+  double price;
+  Popular({
+    required this.imageUrl,
+    required this.title,
+    required this.category,
+    required this.price,
+  });
+}
