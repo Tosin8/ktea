1ff22656b6cd726157ea2d6801e1f9c703c5eb84
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-02-25 19:25:54 +0100

    Update cart_body.dart

--- lib/screens/cart/cart_body.dart
+++ lib/screens/cart/cart_body.dart
@@ -1,4 +1,6 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+
 import 'package:ktea/model/cart_item.dart';
 
 class Cart_Body extends StatefulWidget {
@@ -12,7 +14,46 @@ class _Cart_BodyState extends State<Cart_Body> {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-    body: Padding(
+    body: Column(
+      children: [
+        Column(
+          children: 
+        
+        List.generate(
+          cartItems.length, (index)=> 
+          myCartProductCard
+          (cartItems: cartItems[index],)
+        ), 
+        
+        ),
+        SizedBox(height: 100,), 
+        Container(
+          child: 
+          Row(
+            children: [
+              Text('Total', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+              Text(cartItems.length.toString(), style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+            ]
+          ),
+        )
+      ],
+    ), 
+    ); 
+  }
+}
+
+class myCartProductCard extends StatelessWidget {
+  const myCartProductCard({
+    Key? key,
+    required this.cartItems,
+  }) : super(key: key);
+
+final CartItemModel cartItems; 
+
+@override
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Column(
         children: [
@@ -25,25 +66,25 @@ class _Cart_BodyState extends State<Cart_Body> {
                     borderRadius: BorderRadius.circular(8),
                     image: DecorationImage(
                       image: AssetImage(
-                        cartItems[0].image
+                        cartItems.image
                       ), fit: BoxFit.cover)),
                 ), 
-                SizedBox(width: 10),
+                const SizedBox(width: 10),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(cartItems[0].productId),
-                    Text(cartItems[0].name, style: TextStyle(fontWeight: FontWeight.w400, fontSize: 18),),
-                    Text(cartItems[0].cost.toString(), 
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),),
+                    Text(cartItems.productId),
+                    Text(cartItems.name, style: const TextStyle(fontWeight: FontWeight.w400, fontSize: 18),),
+                    Text(cartItems.cost.toString(), 
+                    style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),),
                   ],
                 ), 
-                SizedBox(width: 20,), 
+                const SizedBox(width: 20,), 
                 Row(
                   children: [
-                    IconButton(onPressed: (){}, icon: Icon(Icons.remove_circle)),
-                    Text(cartItems[0].quantity.toString()),
-                    IconButton(onPressed: (){}, icon: Icon(Icons.add_circle))
+                    IconButton(onPressed: (){}, icon: const Icon(Icons.remove_circle)),
+                    Text(cartItems.quantity.toString()),
+                    IconButton(onPressed: (){}, icon: const Icon(Icons.add_circle))
                   ],
                 )
               ],
@@ -51,8 +92,6 @@ class _Cart_BodyState extends State<Cart_Body> {
           )
         ],
       ),
-    )
-        
-    ); 
+    );
   }
 }
