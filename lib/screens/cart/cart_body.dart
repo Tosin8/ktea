71008825b55ec2d95a66d2a4ae91f90e6984fc74
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-02-25 19:10:08 +0100

    Creating Cart UI

--- lib/screens/cart/cart_body.dart
+++ lib/screens/cart/cart_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ktea/model/cart_item.dart';
 
 class Cart_Body extends StatefulWidget {
   const Cart_Body({super.key});
@@ -11,20 +12,33 @@ class _Cart_BodyState extends State<Cart_Body> {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-    body: Column(
-      children: [
-        Container(
-          child: Row(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage(''))),
-              )
-            ],
-          ),
-        )
-      ],
+    body: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Container(
+            child: Row(
+              children: [
+                Container(
+                  height: 100, width: 100, 
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    image: DecorationImage(
+                      image: AssetImage(
+                        cartItems[0].image
+                      ), fit: BoxFit.cover)),
+                ), 
+                SizedBox(width: 10),
+                Column(
+                  children: [
+                    Text(cartItems[0].name),
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     )
         
     ); 
