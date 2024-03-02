e618cddfea8811777ee9a53f90c718a96dd644f0
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-02 16:38:57 +0100

    Update dining_body.dart

--- lib/screens/categories/dining/dining_body.dart
+++ lib/screens/categories/dining/dining_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:ktea/model/dining.dart';
 
 import 'dining_body_widget.dart';
 
@@ -20,15 +21,77 @@ class _DiningScreenState extends State<DiningScreen> {
     return Scaffold(
       body: ListView(
         children:  [
-           header(),
-           SizedBox(height: 5), 
-           Container(
-            height: 40, 
-            width: 60,
-            decoration: BoxDecoration(
-              color: Colors.white, 
-              borderRadius: BorderRadius.circular(20), 
-            ),
+           const header(),
+           const SizedBox(height: 5), 
+           Card(
+            elevation: 1,
+             child: Container(
+              height: 180, 
+    width: 50, 
+              decoration: BoxDecoration(
+                color: Colors.white, 
+                borderRadius: BorderRadius.circular(5), 
+                image: DecorationImage(image: AssetImage(
+                  diningProducts[0].imgUrl, 
+                  
+                ), 
+                fit: BoxFit.cover, )
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.black.withOpacity(0.2), 
+                      Colors.transparent, 
+                    ], 
+                    begin: Alignment.bottomCenter, 
+                    end: Alignment.centerRight, 
+                  )
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Icon(Icons.favorite_border, color: Colors.white,)
+                      ],
+                    ), 
+                    const SizedBox(height: 20,), 
+                    Text(diningProducts[0].title, 
+                    style: const TextStyle(
+                      color: Colors.white, 
+                      fontSize: 23,
+                       fontWeight: FontWeight.w500, 
+
+                      )), 
+                      const SizedBox(height: 5),
+                      Row(
+                        children: [
+                          Text(diningProducts[0].price.toString(), style: const TextStyle(color: Colors.white, fontWeight: FontWeight.w500, fontSize: 20),
+                          ),
+                          const SizedBox(width: 100),
+                          GestureDetector(
+                            onTap: () {
+                              
+                            },
+                            child: Container(
+                              width: 40, height: 40,
+                              decoration: const BoxDecoration(
+                                shape: BoxShape.circle, 
+                                color: Colors.black, 
+                              ),
+                              child: const Icon(Icons.add, size: 30,color: Colors.white,),
+                            ),
+                          )
+                        ],
+                      ), 
+                      const SizedBox(height: 10,), 
+                  ]
+                ),
+              ),
+             ),
            )
         
         ],
