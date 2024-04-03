d6f22d23526c96a47ba96743c13f32a128273f03
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-04-03 21:23:40 +0100

    Updating

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 
 import 'splash_screen/splash.dart';
@@ -7,19 +6,7 @@ import 'splash_screen/splash.dart';
 
 
  main() {
-  // WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform, 
-  // );
-
-  // await initialization.then((value) => 
-  // Get.put(AppController())); 
-  // //Get.put(UserController()); 
-  // Get.put(ProductsController()); 
-  // // Get.put(CartController()); 
-  // // Get.put(PaymentsController()); 
+  
     
  
   runApp(const MyApp());
