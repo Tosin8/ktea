0e15827951685bb4ca8b7aaf1252bccb7c28e129
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-04-04 23:10:20 +0100

    Create success screen

--- lib/features/auth/screens/otp/otp_btn.dart
+++ lib/features/auth/screens/otp/otp_btn.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:ktea/utils/nav_bar.dart';
+import 'package:ktea/features/auth/screens/success_screen.dart';
 
 class OtpBtn extends StatelessWidget {
   const OtpBtn({
@@ -11,7 +11,7 @@ class OtpBtn extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector( 
       onTap: () {
-        Get.to(() => const NavBarApp());
+        Get.to(() => const SuccessScreen());
       },
       child: Container(width: 350, height: 50, decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.orange), 
       child: const Align(
---
+++ lib/features/auth/screens/success_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class SuccessScreen extends StatefulWidget {
+  const SuccessScreen({super.key});
+
+  @override
+  State<SuccessScreen> createState() => _SuccessScreenState();
+}
+
+class _SuccessScreenState extends State<SuccessScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
