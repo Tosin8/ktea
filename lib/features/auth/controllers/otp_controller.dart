3def1fa3a9c03ca18931b6857a17d749961c48a5
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-04-06 21:30:46 +0100

    Update otp_controller.dart

--- lib/features/auth/controllers/otp_controller.dart
+++ lib/features/auth/controllers/otp_controller.dart
@@ -1,5 +1,13 @@
 import 'package:get/get.dart';
+import 'package:ktea/repository/auth/auth_repository.dart';
+import 'package:ktea/utils/nav_bar.dart';
 
 class OTPController extends GetxController{
   static OTPController get instance => Get.find(); 
+
+
+  void verifyOTP(String otp) async {
+    var isVerified = await AuthenticationRepository.instance.verifyOTP(otp);
+    isVerified ? Get.offAll(const NavBarApp()) : Get.back(); 
+  }
 }
