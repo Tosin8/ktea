b6b57bd4c703bad31903a348dd938a352c65d00d
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-04-06 10:42:54 +0100

    OptionResets

---
+++ lib/features/auth/screens/forgot_pass/option_reset.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+class OptionResetBtn extends StatelessWidget {
+  const OptionResetBtn({
+    super.key, 
+    required this.btnIcon,
+     required this.title, 
+     required this.subTitle, 
+     required this.onTap,
+  });
+
+
+final IconData btnIcon; 
+final String title, subTitle; 
+final VoidCallback onTap; 
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector( 
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(20.0), 
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0), 
+        color: Colors.grey.shade200),
+        child: Row(
+          children: [
+             Icon(btnIcon, color: Colors.black,size: 30,),
+            const SizedBox(width: 20,), 
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(title, style: TextStyle(color: Colors.black.withOpacity(0.8), fontSize: 16),),
+                Text(subTitle, style: TextStyle(color: Colors.black.withOpacity(0.8), fontSize: 14),),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
--- lib/features/auth/screens/signin/forget_psw_btn.dart
+++ lib/features/auth/screens/signin/forget_psw_btn.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:ktea/features/auth/screens/forgot_pass/forgot.dart';
+import 'package:ktea/features/auth/screens/forgot_pass/option_reset.dart';
 
 class fgtPassword extends StatelessWidget {
   const fgtPassword({
@@ -9,14 +11,48 @@ class fgtPassword extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap:() {
-        Get.to(() => const ForgotPwd()); 
-      },
-      child: Text('Forgot Password? ',
-       style: TextStyle(color: Colors.black.withOpacity(0.8),
-        fontSize: 14),),
-    );
+    return TextButton(
+            onPressed: (){
+            showModalBottomSheet(
+              context: context,
+               builder: (context) => Container(
+                // width: MediaQuery.of(context).size.width,
+                width: 600,
+              padding: const EdgeInsets.all(30), 
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+const Text('Make Selection!', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+), 
+const SizedBox(height: 5,), 
+const Text('Select one of the options given below to\nreset your password', 
+style: TextStyle(
+  color: Colors.black, 
+  fontSize: 14),
+  ), 
+  const SizedBox(height: 30.0,), 
+  OptionResetBtn(
+    btnIcon: Iconsax.sms,
+     title: 'E-Mail', 
+     subTitle: 'Reset via E-Mail Verification',
+     onTap: () {},
+     ), 
+SizedBox(height: 20,), 
+     OptionResetBtn(
+    btnIcon: Iconsax.mobile,
+     title: 'Phone', 
+     subTitle: 'Reset via Phone Verification',
+     onTap: () {},
+     ),
+                ],
+              ),
+            )); 
+          }, child: 
+          Text('Forgot Password?', 
+          style: TextStyle(
+            color: Colors.black.withOpacity(0.8),
+           fontSize: 14),)
+          ); 
   }
 }
 
