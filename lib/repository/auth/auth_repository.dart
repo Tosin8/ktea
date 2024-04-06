3227c71bc313d753c6beb261161f94dbebcc3f46
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-04-06 03:12:16 +0100

    Update auth_repository.dart

--- lib/repository/auth/auth_repository.dart
+++ lib/repository/auth/auth_repository.dart
@@ -1,16 +1,24 @@
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
+import 'package:ktea/features/auth/screens/onboarding/splash.dart';
+import 'package:ktea/utils/nav_bar.dart';
 
 class AuthenticationRepository extends GetxController{
   static AuthenticationRepository get instance => Get.find();
 
   // Variables
   final _auth = FirebaseAuth.instance;
-  final Rx<User?> firebaseUser; 
+ late final Rx<User?> firebaseUser; 
 
   @override 
   void onReady() {
-    
+    firebaseUser = Rx<User?>(_auth.currentUser); 
+    firebaseUser.bindStream(_auth.userChanges());
+    ever(firebaseUser, _setInitialScreen); 
+  }
+
+  _setInitialScreen(User? user) {
+    user == null ? Get.offAll(() => const Onboarding()): Get.offAll(() => const NavBarApp());  
   }
 }
