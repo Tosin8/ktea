145a81680815618c8e4bdf87453266b241830452
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-04-06 03:01:39 +0100

    Update auth_repository.dart

--- lib/repository/auth/auth_repository.dart
+++ lib/repository/auth/auth_repository.dart
@@ -2,8 +2,12 @@
 
 
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 
 class AuthenticationRepository extends GetxController{
   static AuthenticationRepository get instance => Get.find();
+
+  // Variables
+  final _auth = FirebaseAuth.instance;
 }
