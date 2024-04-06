254150035066c9fa360cbc673e6d7d8891ec043b
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-04-06 10:20:23 +0100

    Update signin_form.dart

--- lib/features/auth/screens/signin/signin_form.dart
+++ lib/features/auth/screens/signin/signin_form.dart
@@ -57,8 +57,48 @@ class _SignInFormState extends State<SignInForm> {
           const LoginBtn(),
           const SizedBox(height: 15,), 
           //const fgtPassword(),
-          TextButton(onPressed: (){
-            showModalBottomSheet(context: context, builder: (context) => Container()); 
+          TextButton(
+            onPressed: (){
+            showModalBottomSheet(
+              context: context,
+               builder: (context) => Container(
+                // width: MediaQuery.of(context).size.width,
+                width: 600,
+              padding: const EdgeInsets.all(30), 
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+const Text('Make Selection!', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+), 
+const SizedBox(height: 5,), 
+const Text('Select one of the options given below to\nreset your password', 
+style: TextStyle(
+  color: Colors.black, 
+  fontSize: 14),
+  ), 
+  const SizedBox(height: 30.0,), 
+  Container(
+    padding: const EdgeInsets.all(20.0), 
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(10.0), 
+    color: Colors.grey.shade200),
+    child: Row(
+      children: [
+        const Icon(Iconsax.sms, color: Colors.black,size: 30,),
+        SizedBox(width: 20,), 
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('E-Mail', style: TextStyle(color: Colors.black.withOpacity(0.8), fontSize: 16),),
+            Text('Reset via E-Mail Verification', style: TextStyle(color: Colors.black.withOpacity(0.8), fontSize: 14),),
+          ],
+        )
+      ],
+    ),
+  ), 
+                ],
+              ),
+            )); 
           }, child: 
           Text('Forgot Password?', style: TextStyle(color: Colors.black.withOpacity(0.8), fontSize: 14),)),
           
