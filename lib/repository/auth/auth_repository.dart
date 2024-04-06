35d34257fcbe72a2c76f4397ec4bea394b560fee
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-04-06 03:29:44 +0100

    Update auth_repository.dart

--- lib/repository/auth/auth_repository.dart
+++ lib/repository/auth/auth_repository.dart
@@ -21,4 +21,25 @@ class AuthenticationRepository extends GetxController{
   _setInitialScreen(User? user) {
     user == null ? Get.offAll(() => const Onboarding()): Get.offAll(() => const NavBarApp());  
   }
+
+  Future<void> createUserWithEmailAndPassword(String email, String password) async {
+
+    try{
+   await _auth.createUserWithEmailAndPassword(email: email, password: password); 
+  } on FirebaseAuthException catch (e) {
+    
+  } catch (_){}
+}
+
+
+Future<void> loginWithEmailAndPassword(String email, String password) async {
+
+    try{
+   await _auth.signInWithEmailAndPassword(email: email, password: password); 
+  } on FirebaseAuthException catch(e){
+    
+  } catch (_){}
+}
+
+  Future<void> logOut() async => await _auth.signOut();
 }
