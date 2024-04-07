4578b7046da8315528297df1c55049302d0f9afc
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-04-07 21:08:11 +0100

    Update user_model.dart

--- lib/features/auth/model/user_model.dart
+++ lib/features/auth/model/user_model.dart
@@ -1,3 +1,24 @@
 class UserModel {
-  
+  final String? id; 
+  final String fullName; 
+  final String email;
+  final String phoneNo; 
+  final String password;
+
+  UserModel({
+    required this.id, 
+    required this.fullName, 
+    required this.email, 
+    required this.phoneNo, 
+    required this.password
+    }); 
+
+    toJson() {
+      return {
+        'FullName': fullName, 
+        'Email': email,
+        'Phone': phoneNo, 
+        'Passworod': password, 
+      }; 
+    }
 }
