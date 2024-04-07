d8ffaf0c8b7c821977b25b089daa03ea891fc50d
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-04-07 21:59:00 +0100

    Update signup_controller.dart

--- lib/features/auth/controllers/signup_controller.dart
+++ lib/features/auth/controllers/signup_controller.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:ktea/repository/auth/auth_repository.dart';
+import 'package:ktea/repository/user/user_repository.dart';
+
+import '../model/user_model.dart';
 
 class SignUpController extends GetxController{
   static SignUpController get instance => Get.find();
@@ -10,6 +13,8 @@ final  email = TextEditingController();
 final name = TextEditingController(); 
   final password = TextEditingController(); 
   final phone = TextEditingController();
+
+  final userRepo = Get.put(UserRepository()); 
 
 
 // Function call when user click on signup button
@@ -23,6 +28,12 @@ void registerUser(String email, String password) {
 
 // Get phoneNo from user and pass it to Auth Repo for Firebase Auth. 
 // Auth Repo will call firebase auth and send OTP to user.
+
+void createUser(UserModel user) {
+  userRepo.createUser(user); 
+  }
+
+  
 
 void phoneAuthentication(String phoneNo) {
   AuthenticationRepository.instance.phoneAuthentication(phoneNo);}
