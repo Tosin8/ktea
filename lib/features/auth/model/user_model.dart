3350145722c1a354327fc3bb252f4e38653a15c6
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-04-14 19:55:45 +0100

    Create json files

--- lib/features/auth/model/user_model.dart
+++ lib/features/auth/model/user_model.dart
@@ -1,4 +1,4 @@
-import 'dart:html';
+
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 
--- lib/features/auth/screens/widget.dart
+++ lib/features/auth/screens/widget.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:iconsax/iconsax.dart';
 
 
