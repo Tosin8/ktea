1501ce8e145b4c4d302df71f8f43372cc099a563
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-04-15 15:41:28 +0100

    Update signup_controller.dart

--- lib/features/auth/controllers/signup_controller.dart
+++ lib/features/auth/controllers/signup_controller.dart
@@ -1,5 +1,43 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class SignUpController extends GetxController{
 static SignUpController get instance => Get.find();
+
+/// Variables
+final email = TextEditingController(); 
+final firstName = TextEditingController();
+final lastName = TextEditingController();
+final phoneNumber = TextEditingController();
+final userName = TextEditingController();
+final password = TextEditingController();
+
+/// Sign Up Methods
+
+Future<void> signUp() async {
+  try {
+
+// Start Loading 
+
+// Check Internet Connectivity 
+
+// Form Validation 
+
+// Privacy Policy Check
+
+// Register User in the firebase Auth.
+
+// save auth user data in the fire store
+
+// show success message. 
+
+// move to verify email screen. 
+
+
+  } catch (e) {
+    
+  } finally {
+    // Callback when the process is finished,
+  }
+}
 }
