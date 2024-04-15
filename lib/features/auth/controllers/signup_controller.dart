2e18ccf384db6f533ed08060d8e55c1abee844c6
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-04-15 15:10:01 +0100

    Update signup_controller.dart

--- lib/features/auth/controllers/signup_controller.dart
+++ lib/features/auth/controllers/signup_controller.dart
@@ -1,48 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
-import 'package:ktea/features/auth/screens/forgot_pass/otp/otp.dart';
-import 'package:ktea/repository/auth/auth_repository.dart';
-import 'package:ktea/repository/user/user_repository.dart';
-
-import '../model/user_model.dart';
 
 class SignUpController extends GetxController{
-  static SignUpController get instance => Get.find();
-
-  // Text field controllers
-final  email = TextEditingController(); 
-final name = TextEditingController(); 
-  final password = TextEditingController(); 
-  final phone = TextEditingController();
-
-  final userRepo = Get.put(UserRepository()); 
-
-
-// Function call when user click on signup button
-void registerUser(String email, String password) {
-  String? error = AuthenticationRepository.instance.createUserWithEmailAndPassword(email, password) as String?; 
-  if(error != null) {
-    Get.showSnackbar(GetSnackBar(message: error.toString())); 
-  }
-   
+static SignUpController get instance => Get.find();
 }
-
-// Get phoneNo from user and pass it to Auth Repo for Firebase Auth. 
-// Auth Repo will call firebase auth and send OTP to user.
-
-
-// Getting phoneno from user and pass it to Auth. Repo for firebase Auth. 
-
-void createUser(UserModel user) async {
-  await userRepo.createUser(user); 
-  phoneAuthentication(user.phoneNo);
-  Get.to(() => const OTP());
-  }
-
-  
-
-void phoneAuthentication(String phoneNo) {
-  AuthenticationRepository.instance.phoneAuthentication(phoneNo);}
-
-  }
-
